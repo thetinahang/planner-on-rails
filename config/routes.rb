0d1289d7a16be4b14ae2c8222e860c4861d15cab
thetinahang <thetinahang@gmail.com>
2018-07-23 18:33:57 -0700

    refactor hours and months forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :days, only: [:index, :show, :new, :create, :edit]
   patch 'days/:id', to: 'days#update'
+  resources :hours, only: [:index, :show, :new, :create, :edit]
+  patch 'hours/:id', to: 'hours#update'
+  resources :months, only: [:index, :show, :new, :create, :edit]
+  patch 'months/:id', to: 'months#update'
+  resources :weeks, only: [:index, :show, :new, :create, :edit]
+  patch 'weeks/:id', to: 'weeks#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
