d7423e9325e7dfeadf29330b51f0a4103cf4faff
thetinahang <thetinahang@gmail.com>
2018-07-27 19:39:48 -0700

    edited due to server errors

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,6 +1,6 @@
 class Week < ActiveRecord::Base
 	has_many :days
-	has_many :hours, through :days
+	has_many :hours, through: :days
 	belongs_to :month
 	validates :week_day_date, presence: true, allow_blank: false
 end
