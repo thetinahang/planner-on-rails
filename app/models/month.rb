ce190a52cd21736b36cc21ec869c9357c7337097
thetinahang <thetinahang@gmail.com>
2018-07-29 15:57:07 -0700

    month works

--- app/models/month.rb
+++ app/models/month.rb
@@ -1,6 +1,6 @@
 class Month < ActiveRecord::Base
 	has_many :weeks
 	has_many :days, through: :weeks
-	belongs_to :user 
+	belongs_to :user, optional: true #need to figure out this part
 	validates :month_name, presence: true
 end
