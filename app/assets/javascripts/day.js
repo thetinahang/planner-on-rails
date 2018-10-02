d3d4f06928140e5264132b739a2675cb6455e311
thetinahang <thetinahang@gmail.com>
2018-10-01 21:01:39 -0700

    move js index and show code to day js file

--- app/assets/javascripts/day.js
+++ app/assets/javascripts/day.js
@@ -1,30 +1,83 @@
+document.addEventListener("turbolinks:load", function() {
+  indexShow();
+  showDay();
+})
 
-$(function() {
-  indexShow()
-});
+
+//$(function() {
+//  
+//});
 
 function indexShow() {
 	$(".js-more").on("click", function() {
 	    var id = $(this).data("id");
 	    $.get("/days/" + id + ".json", function(response) {
 	        var dayFocus = response["day_focus"];
-	        var dayExercise = response["day_exercise"];//
-
-
+	        var dayExercise = response["day_exercise"];
 	        var hour = response["hours"].map(function (hour) {
-	          return hour["hour_number"] + ": "});//
-
+	          return hour["hour_number"] + ": "});
 	        var task = response["tasks"].map(function (task) {
-	          return task["task_content"]});//
-
+	          return task["task_content"]});
 	        var hourwithtask = hour.map(function(e, i) {
 	          return "<li>" + e + task[i] + "</li>"}).join('');
-	        
 	        var dayText = "<p> Focus: "+ dayFocus + "</p><p> Exercise: " + dayExercise + "</p><ul>" + hourwithtask + "</ul>" ;//
-
 	        $("#day-" + id).html(dayText);
 	    });
 	});
+}
+
+//$(function() {
+//  
+//});
+
+function showDay() {
+	$(".js-next").on("click", function() {
+   	 $(".test").hide();
+     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+     $.get("/days/" + nextId + ".json", function(data) {
+       $(".dayDayDate").text(data["day_day_date"]);
+       $(".dayGrateful1").text(data["day_grateful_1"]);
+       $(".dayGrateful2").text(data["day_grateful_2"]);
+       $(".dayGrateful3").text(data["day_grateful_3"]);
+       $(".dayExcited1").text(data["day_excited_1"]);
+       $(".dayExcited2").text(data["day_excited_2"]);
+       $(".dayExcited3").text(data["day_excited_3"]);
+       $(".dayAffirmation").text(data["day_affirmation"]);
+       $(".dayFocus").text(data["day_focus"]);
+       $(".dayExercise").text(data["day_exercise"]);
+       $(".dayPriority1").text(data["day_priority_1"]);
+       $(".dayPriority2").text(data["day_priority_2"]);
+       $(".dayPriority3").text(data["day_priority_3"]);
+       $(".dayPriority4").text(data["day_priority_4"]);
+       $(".dayPriority5").text(data["day_priority_5"]);
+        var hour = data["hours"].map(function (hour) {
+          return hour["hour_number"] + ": "});
+        var task = data["tasks"].map(function (task) {
+          return task["task_content"]});
+        var hourwithtask = hour.map(function(e, i) {
+          return e + task[i] + "<br>"}).join('');
+        
+        var dayText = hourwithtask;
+        $(".hourTask").html(hourwithtask);
+       $(".dayTask1").text(data["day_task_1"]);
+       $(".dayTask2").text(data["day_task_2"]);
+       $(".dayTask3").text(data["day_task_3"]);
+       $(".dayTask4").text(data["day_task_4"]);
+       $(".dayTask5").text(data["day_task_5"]);
+       $(".dayTask6").text(data["day_task_6"]);
+       $(".dayTask7").text(data["day_task_7"]);
+       $(".dayTask8").text(data["day_task_8"]);
+       $(".dayTask9").text(data["day_task_9"]);
+       $(".dayTask10").text(data["day_task_10"]);
+       $(".dayNotes").text(data["day_notes"]);
+       $(".dayWin1").text(data["day_win_1"]);
+       $(".dayWin2").text(data["day_win_2"]);
+       $(".dayWin3").text(data["day_win_3"]);
+       $(".dayImprovement").text(data["day_improvement"]);
+       // re-set the id to current on the link
+       $(".js-next").attr("data-id", data["id"]);
+     });
+   });
 }
 
 class Day {
