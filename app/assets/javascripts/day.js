5e20722553a14413c23878053bf172d79d9b5a35
thetinahang <thetinahang@gmail.com>
2018-10-01 20:04:19 -0700

    add method to constructor function

--- app/assets/javascripts/day.js
+++ app/assets/javascripts/day.js
@@ -7,39 +7,47 @@
 //}
 // 
 
-function Day(day_day_date, user_id, day_grateful_1, day_grateful_2, day_grateful_3, day_excited_1, day_excited_2, day_excited_3, day_affirmation, day_focus, day_exercise, day_priority_1, day_priority_2, day_priority_3, day_priority_4, day_priority_5, day_task_1, day_task_2, day_task_3, day_task_4, day_task_5, day_task_6, day_task_7, day_task_8, day_task_9, day_task_10, day_notes, day_win_1, day_win_2, day_win_3, tasks) {
-	this.day_day_date = day_day_date;
-	this.user_id = user_id; 
-    this.day_grateful_1 = day_grateful_1; 
-    this.day_grateful_2 = day_grateful_2;
-    this.day_grateful_3 = day_grateful_3;
-    this.day_excited_1 = day_excited_1;
-    this.day_excited_2 = day_excited_2;
-    this.day_excited_3 = day_excited_3;
-    this.day_affirmation = day_affirmation;
-    this.day_focus = day_focus;
-    this.day_exercise = day_exercise;
-    this.day_priority_1 = day_priority_1;
-    this.day_priority_2 = day_priority_2;
-    this.day_priority_3 = day_priority_3;
-    this.day_priority_4 = day_priority_4;
-    this.day_priority_5 = day_priority_5;
-    this.day_task_1 = day_task_1;
-    this.day_task_2 = day_task_2;
-    this.day_task_3 = day_task_3;
-    this.day_task_4 = day_task_4;
-    this.day_task_5 = day_task_5;
-    this.day_task_6 = day_task_6;
-    this.day_task_7 = day_task_7;
-    this.day_task_8 = day_task_8;
-    this.day_task_9 = day_task_9;
-    this.day_task_10 = day_task_10;
-    this.day_notes = day_notes;
-    this.day_win_1 = day_win_1;
-    this.day_win_2 = day_win_2;
-    this.day_win_3 = day_win_3;
-    this.tasks = tasks;
+class Day {
+	constructor(id, day_day_date, user_id, day_grateful_1, day_grateful_2, day_grateful_3, day_excited_1, day_excited_2, day_excited_3, day_affirmation, day_focus, day_exercise, day_priority_1, day_priority_2, day_priority_3, day_priority_4, day_priority_5, day_task_1, day_task_2, day_task_3, day_task_4, day_task_5, day_task_6, day_task_7, day_task_8, day_task_9, day_task_10, day_notes, day_win_1, day_win_2, day_win_3, tasks = []){
+		this.id = id;
+		this.day_day_date = day_day_date;
+		this.user_id = user_id; 
+	    this.day_grateful_1 = day_grateful_1; 
+	    this.day_grateful_2 = day_grateful_2;
+	    this.day_grateful_3 = day_grateful_3;
+	    this.day_excited_1 = day_excited_1;
+	    this.day_excited_2 = day_excited_2;
+	    this.day_excited_3 = day_excited_3;
+	    this.day_affirmation = day_affirmation;
+	    this.day_focus = day_focus;
+	    this.day_exercise = day_exercise;
+	    this.day_priority_1 = day_priority_1;
+	    this.day_priority_2 = day_priority_2;
+	    this.day_priority_3 = day_priority_3;
+	    this.day_priority_4 = day_priority_4;
+	    this.day_priority_5 = day_priority_5;
+	    this.day_task_1 = day_task_1;
+	    this.day_task_2 = day_task_2;
+	    this.day_task_3 = day_task_3;
+	    this.day_task_4 = day_task_4;
+	    this.day_task_5 = day_task_5;
+	    this.day_task_6 = day_task_6;
+	    this.day_task_7 = day_task_7;
+	    this.day_task_8 = day_task_8;
+	    this.day_task_9 = day_task_9;
+	    this.day_task_10 = day_task_10;
+	    this.day_notes = day_notes;
+	    this.day_win_1 = day_win_1;
+	    this.day_win_2 = day_win_2;
+	    this.day_win_3 = day_win_3;
+	    this.tasks = tasks;
+	};
+
+	dayAffirmationAlert() {
+		alert(this.day_affirmation);
+	}
 }
+
 //User.prototype.sayHello = function() {
 //  console.log("Hello, my name is "+ this.name);
 //}
